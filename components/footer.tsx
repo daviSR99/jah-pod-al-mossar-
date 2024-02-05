eb6c016b69fd69dc631b8055bdec6f7f11b0c78a
Davi de S.R <findingtheflametowin@gmail.com>
2024-02-04 23:01:52 -0300

    Teste

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,7 @@ export default function Footer() {
   return (
    <footer className='mb-10 px-4 text-center text-custom-gray text-bold '>
     <div className='flex items-center justify-center mb-5 gap-6'>
-    <a href="https://www.linkedin.com/in/d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f/" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin  size={43}/>
-        </a>
-        
+      
         <a href="https://github.com/daviSR99" target="_blank" rel="noopener noreferrer">
           <FaGithub size={40}/>
         </a>
